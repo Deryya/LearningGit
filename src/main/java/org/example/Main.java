b235ae0447d34d43544d23b31850efcf7b8a9c7e
DeryaBaykal <derya.baykal91@gmail.com>
2022-08-22 13:55:25 +0200

    added.gitignore file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world! \n Change 3!");
+        System.out.println("Hello world! \n This is the 4th change in the file.");
     }
 }
