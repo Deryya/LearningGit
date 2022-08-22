c755ce98e657350ac00a2567f09aae76f63117e7
DeryaBaykal <derya.baykal91@gmail.com>
2022-08-22 15:43:02 +0200

    preparation for merging

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,6 @@ public class Main {
 
         System.out.println("Hello world! \n This is the 4th change in the file.");
         System.out.println("New changes to the cloned Github project!");
+        System.out.println("Adding another change to try merging");
     }
 }
